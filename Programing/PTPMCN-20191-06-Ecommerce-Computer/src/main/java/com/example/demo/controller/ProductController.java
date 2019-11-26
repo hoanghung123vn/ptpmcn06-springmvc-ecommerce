cbdb40acc7ce0412e7be36a0bbb3c3bdf9d65f42
leminhnguyen <leminhnguyen.bk@gmail.com>
2019-11-26 17:20:30 +0700

    updated

--- Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/controller/ProductController.java
+++ Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
-public class ProductController {
-	
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.entity.Product;
+
+@Controller
+public interface ProductController extends CrudRepository<Product, Integer>{
+	Product findByProductName(String name);
+	List<Product> findByIdManufacturer(Integer idManufacturer);
+	List<Product> findByIdType(Integer idType);
 }
