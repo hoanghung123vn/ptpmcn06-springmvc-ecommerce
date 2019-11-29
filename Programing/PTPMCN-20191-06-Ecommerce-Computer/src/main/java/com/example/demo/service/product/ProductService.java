598193ecdd9346e83b0fc2789dfd43e3ccbad7df
leminhnguyen <leminhnguyen.bk@gmail.com>
2019-11-29 16:59:35 +0700

    completed view product

--- Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/service/product/ProductService.java
+++ Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/service/product/ProductService.java
@@ -8,4 +8,5 @@ public interface ProductService {
 	Product findByCode(Integer code);
 	List<Product> findAll();
 	void deleteByCode(Integer id);
+	void addProduct(Product product);
 }
--- Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/service/product/ProductServiceImpl.java
+++ Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/service/product/ProductServiceImpl.java
@@ -2,14 +2,17 @@ package com.example.demo.service.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dao.ProductRepository;
 import com.example.demo.entity.Product;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 	
-	
+	@Autowired
+	ProductRepository productRepository;
 	
 	@Override
 	public Product findByCode(Integer code) {
@@ -19,32 +22,18 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public List<Product> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return productRepository.findAll();
 	}
-
-	@Override
-	public void deleteByCode(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-    @Override
-    public Product findByCode(Integer code) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public List<Product> findAll() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+	
     @Override
     public void deleteByCode(Integer id) {
         // TODO Auto-generated method stub
         
     }
+
+	@Override
+	public void addProduct(Product product) {
+		
+	}
 
 }
