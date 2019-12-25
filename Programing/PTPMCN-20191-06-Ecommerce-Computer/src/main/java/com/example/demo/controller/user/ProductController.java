585932ffc4e34a6d333dcdba99f9099cde90ac56
leminhnguyen <leminhnguyen.bk@gmail.com>
2019-12-25 10:52:34 +0700

    add view-details and view-quick method

--- Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/controller/user/ProductController.java
+++ Programing/PTPMCN-20191-06-Ecommerce-Computer/src/main/java/com/example/demo/controller/user/ProductController.java
@@ -4,26 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.dao.OrderRepository;
+import com.example.demo.dao.OrdersRepository;
 import com.example.demo.service.product.ProductService;
 
 @Controller
-@RequestMapping(path= "/user")
+@RequestMapping(path= "")
 public class ProductController {
 	
 	@Autowired
 	ProductService productService;
 	
 	@Autowired
-	OrderRepository orderRepository;
+	OrdersRepository ordersRepository;
 	
-	@GetMapping("/products")
-	public String listProduct(Model model) {
-		orderRepository.findAll();
-		model.addAttribute("products", productService.findAll());
-		return "user/index";
+	@GetMapping("/view-details/{code}/")
+	public String viewDetails(Model model, @PathVariable("code") String code) {
+		System.out.println(code);
+		int pcode = Integer.parseInt(code);
+		model.addAttribute("product",  productService.findByCode(pcode));
+		return "user/single_product";
+	}
+
+	@GetMapping("/view-quick/{code}/")
+	public String viewQuick(Model model, @PathVariable("code") String code) {
+		 System.out.println(code);
+		 int pcode = Integer.parseInt(code);
+		 model.addAttribute("product",  productService.findByCode(pcode));
+		return "user/quick_view";
 	}
 	
 	
